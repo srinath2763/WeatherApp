ad551762ae1182f16dd73fc5bd98dc5636a3dd89
IMCS2 <IMCS2@imcs1s-macbook-air-2.local>
2019-02-16 22:56:10 -0600

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by IMCS2 on 2/16/19.
+//  Copyright © 2019 IMCS2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
